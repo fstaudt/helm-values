5c65fb7befe1d2bd7cdb6ec6998675c9a2735244
Francois Staudt <francois.staudt@soprabanking.com>
2022-09-16 14:38:52 +0200

    build: bump dependency versions
    - jackson-databind bumped from 2.13.3 to 2.13.4
    - jackson-module-kotlin bumped from 2.13.3 to 2.13.4
    - jackson-dataformat-yaml bumped from 2.13.3 to 2.13.4
    - wiremock-jre8 bumped from 2.33.2 to 2.34.0

--- helm-values-shared/build.gradle.kts
+++ helm-values-shared/build.gradle.kts
@@ -8,13 +8,13 @@ plugins {
 dependencies {
     api("org.apache.httpcomponents.client5:httpclient5:5.1.3")
     api("org.apache.commons:commons-compress:1.21")
-    api("com.fasterxml.jackson.core:jackson-databind:2.13.3")
-    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
+    api("com.fasterxml.jackson.core:jackson-databind:2.13.4")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
     api("com.github.java-json-tools:json-patch:1.13")
     testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testImplementation("com.github.tomakehurst:wiremock-jre8:2.33.2")
+    testImplementation("com.github.tomakehurst:wiremock-jre8:2.34.0")
     testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.35.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
