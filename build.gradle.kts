230cf6602c6f8c7139dd8aa68fa4319a2f21218d
fstaudt <fstaudt@gmail.com>
2022-09-17 16:43:39 +0200

    build: publish helm-values-shared to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,21 @@ subprojects {
         mavenCentral()
     }
 }
-
+repositories {
+    mavenCentral()
+}
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+val sonatypeUsername: String by project
+val sonatypePassword: String by project
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(sonatypeUsername)
+            password.set(sonatypePassword)
+        }
+    }
+}
--- helm-values-shared/build.gradle.kts
+++ helm-values-shared/build.gradle.kts
@@ -1,8 +1,11 @@
 import org.gradle.api.JavaVersion.VERSION_1_8
+import java.net.URI
 
 plugins {
     kotlin("jvm") version embeddedKotlinVersion
+    id("org.jetbrains.dokka") version "1.7.10"
     `maven-publish`
+    signing
 }
 
 dependencies {
@@ -31,14 +34,58 @@ val sourcesJar = tasks.register<Jar>("sourcesJar") {
     archiveClassifier.set("sources")
     from(sourceSets.main.get().allSource)
 }
+val javadocJar = tasks.register<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.getByName("dokkaHtml").outputs)
+}
 
 publishing {
     publications {
         register<MavenPublication>("mavenJava").configure {
+            pom {
+                name.set("${project.group}.${project.name}")
+                description.set("Utility classes to generate JSON schema for values of Helm charts")
+                url.set("https://github.com/fstaudt/helm-values")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("François Staudt")
+                        email.set("fstaudt@gmail.com")
+                        url.set("https://github.com/fstaudt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/fstaudt/helm-values.git")
+                    developerConnection.set("scm:git:ssh://github.com:fstaudt/helm-values.git")
+                    url.set("https://github.com/fstaudt/helm-values/tree/main")
+                }
+            }
             from(components["java"])
             artifact(sourcesJar)
+            artifact(javadocJar)
             groupId = "${project.group}"
             artifactId = project.name
         }
     }
+    repositories {
+        val sonatypeUsername: String by project
+        val sonatypePassword: String by project
+        maven {
+            url = URI("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = sonatypeUsername
+                password = sonatypePassword
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
 }
