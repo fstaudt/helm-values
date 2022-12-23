65ffd35a9b7dbfdedd4103f0a944aea2553ec8ce
fstaudt <fstaudt@gmail.com>
2022-12-23 17:30:13 +0100

    build(deps): bump intellij gradle plugin from 1.10.0 to 1.11.0

--- helm-values-intellij-plugin/build.gradle.kts
+++ helm-values-intellij-plugin/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     // Kotlin support
     kotlin("jvm")
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.intellij") version "1.11.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
 }
