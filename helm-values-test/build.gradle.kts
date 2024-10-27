3b44aead5e47865d113c4b264f627ad3855b97ea
fstaudt <fstaudt@gmail.com>
2024-10-27 10:17:52 +0100

    build: bump wiremock to 3.9.2

--- helm-values-test/build.gradle.kts
+++ helm-values-test/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     api("org.junit.jupiter:junit-jupiter-api:5.11.0")
     api("org.assertj:assertj-core:3.26.3")
     api("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
-    api("com.github.tomakehurst:wiremock-jre8:3.0.1")
+    api("org.wiremock:wiremock:3.9.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     testRuntimeOnly("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
