8d56e1e210ce8b0e23287d000716d627591d09b8
fstaudt <fstaudt@gmail.com>
2024-10-27 10:01:55 +0100

    build: bump json-unit-assertj to 3.2.7

--- helm-values-test/build.gradle.kts
+++ helm-values-test/build.gradle.kts
@@ -10,9 +10,7 @@ kotlin {
 dependencies {
     api("org.junit.jupiter:junit-jupiter-api:5.11.0")
     api("org.assertj:assertj-core:3.26.3")
-    api("net.javacrumbs.json-unit:json-unit-assertj:2.38.0") {
-        api("net.minidev:json-smart:2.5.1")
-    }
+    api("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
     api("com.github.tomakehurst:wiremock-jre8:3.0.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     testRuntimeOnly("com.fasterxml.jackson.core:jackson-databind:2.17.2")
